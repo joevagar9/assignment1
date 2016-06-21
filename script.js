a7333ddab656a2d0e4f4729f1b252fe8e10676ee
Jose Vargas <vargas1530@gmail.com>
2016-06-21 00:10:16 -0700

    script

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+//step1
+    someMonth
+    function theMonth
+    currentMonth
+    summerMonth
+    myLibraryAFunction
+    
+//step 2
+    20 
+    "literal"
+    true  
+    null 
+    
+//step 3 
+    var totalSum = 3 (2 - 1) *1000;
+    var totalSales = store1 +store2,
+    
+//step 4 
+     strFirstName;
+     strLastName;
+     strAddress;
+     strCity;
+     strState;
+     numZipCode;
+     numYourAge;
+     strReferralSource;
+     boolMayWeContactYou; 
+     
+//step 5
+     var strFirstName = "Jose Vargas"
+     var isInstructor = "Emily Kay"
+     var strCity = "San Diego"
+     
